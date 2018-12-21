42657cb41a309c9f3bc38233c7cdc8bb96be68b6
harshit <hg1229@gmail.com>
2018-12-21 16:57:00 +0530

    Added the functionality to change fonts.

--- app/src/main/java/com/example/mercury/my_code/Scroller.java
+++ app/src/main/java/com/example/mercury/my_code/Scroller.java
@@ -1,7 +1,10 @@
 package com.example.mercury.my_code;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class Scroller extends AppCompatActivity {
 
@@ -9,6 +12,12 @@ public class Scroller extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroller);
+    }
+
+    public void change_font(View view){
+        TextView changeFont = (TextView) findViewById(R.id.story_textView); // Assign changeFont to be the object referring to the textView in activity_scroller as id "change_font"
+        Typeface font = Typeface.createFromAsset(getAssets(), "font/myfont.ttf"); // Assign fonts object to refer to the fonts saved in said file, by specifying relative path
+        changeFont.setTypeface(font); // Sets the typeface in the textView to whatever we want.
     }
 }
 
