977595c32f30b7d679fa32261c91c76615324e75
harshit <hg1229@gmail.com>
2018-12-21 11:39:48 +0530

    Added activities.
    Now I know how to add activites and link them. Will post regular updates on git now.

--- app/src/main/java/com/example/mercury/my_code/MainActivity.java
+++ app/src/main/java/com/example/mercury/my_code/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.mercury.my_code;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("MainActivity", "HelloWorld");
+        Log.e("MainActivity", "HelloWorld");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-//    System.out.println("hey");
+/*    public MainActivity activity;
+
+    @Override
+    public void onAttach(Activity activity){
+        this.activity = activity;
+    }*/
+
+
+
+    public void sayHello(View v){
+       Button thebutton = findViewById(R.id.first_button);
+       Intent scroller = new Intent(this, Scroller.class);
+       thebutton.setText(R.string.first_button_change);
+       Toast.makeText(this, "Magic!", Toast.LENGTH_LONG).show();
+       startActivity(scroller);
+    }
+
 }
---
+++ app/src/main/java/com/example/mercury/my_code/Scroller.java
@@ -0,0 +1,15 @@
+package com.example.mercury.my_code;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scroller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroller);
+    }
+}
+
+
